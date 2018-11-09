007344d4b1947aa7d05d05f13c9080c75ca56a67
taru <39840910+taru@users.noreply.github.com>
2018-11-09 16:53:05 -0500

    adding comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// in progress. check counter first.
 
 class App extends Component {
   render() {
