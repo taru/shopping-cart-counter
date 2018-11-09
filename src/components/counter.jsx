7ef52126ac333402296f2d975da1a91a6470e307
taru <39840910+taru@users.noreply.github.com>
2018-11-09 16:33:42 -0500

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  // state is a special property in react components. object that includes any data that this component needs.
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* react frag is so we don't have 1 extra <div> within our root <div> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        {/* m-2 is margin 2 spaces */}
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <div>{this.renderTags()}</div>
+      </React.Fragment>
+    );
+    // return gives React.createElement(parentElement, "content").
+    // this is why we import react at the top
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+// this is standard way to import bootstrap
+import Counter from "./components/counter";
+// bc counter is default export, dont need curly brackets
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
