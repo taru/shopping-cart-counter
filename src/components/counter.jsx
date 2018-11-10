108b7b4a1289e07bac1a7a9d3d0bed292b084834
taru <39840910+taru@users.noreply.github.com>
2018-11-10 01:39:05 -0500

    binding methods

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,18 @@ class Counter extends Component {
   };
   // state is a special property in react components. object that includes any data that this component needs.
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+    // functions are objects, so in the handleincre(), if we use 'this', itll
+    // refer to the function object and not the counter component. the constructor
+    // makes a new instance of handleincrement, which we add to the actual one
+  }
+
+  handleIncrement = () => {
+    console.log("increment clicked", this);
+  };
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
@@ -25,8 +37,17 @@ class Counter extends Component {
         {/* react frag is so we don't have 1 extra <div> within our root <div> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         {/* m-2 is margin 2 spaces */}
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <div>{this.renderTags()}</div>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <div>
+          {this.state.tags.length === 0 && "Please create a new tag"}
+          {/* truesy operand,JS engine will return last operand every time when comparing multiple truesy operands */}
+          {this.renderTags()}
+        </div>
       </React.Fragment>
     );
     // return gives React.createElement(parentElement, "content").
